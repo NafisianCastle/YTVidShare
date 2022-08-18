afb47a2529a69ada77a44a5c37750a8f84efe95b
NafisianCastle <nafis.cseaiub@gmail.com>
2022-08-18 18:41:03 +0600

    added card component for video

---
+++ YTVidShareFrontEnd/ytvidshare/src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    width: 360px;
+`
+
+const Card = () => {
+  return (
+    <Container>
+       <div>hello</div>
+    </Container>
+   
+  )
+}
+
+export default Card
